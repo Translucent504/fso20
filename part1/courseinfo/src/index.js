710e362f541c5a0cc38f007321a9232da9c7b313
translucent504 <fleshlime.1@gmail.com>
2020-06-05 14:29:01 +0500

    step 1 refactoring completed

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({course}) => (
+  <div>
+    <h1>{course}</h1>
+  </div>
+)
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => (
+  <div>
+    <p>
+      {part1} {exercises1}
+    </p>
+    <p>
+      {part2} {exercises2}
+    </p>
+    <p>
+      {part3} {exercises3}
+    </p>
+  </div>
+)
+
+const Total = ({exercises1, exercises2, exercises3}) => (
+  <div>
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  </div>
+)
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,19 +39,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
