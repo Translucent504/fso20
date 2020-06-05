03fe2b952b1516cf4c5101f7412a8dc2faef5cfe
translucent504 <fleshlime.1@gmail.com>
2020-06-05 15:23:40 +0500

    step 4 Refactored App to store all parts into a single array.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -15,41 +15,43 @@ const Part = ({part, exercises}) => (
   </div>
 )
 
-const Content = ({part1, part2, part3}) => (
+const Content = ({parts}) => (
   <div>
-    <Part part={part1.name} exercises={part1.exercises} />
-    <Part part={part2.name} exercises={part2.exercises} />
-    <Part part={part3.name} exercises={part3.exercises} />
+    <Part part={parts[0].name} exercises={parts[0].exercises} />
+    <Part part={parts[1].name} exercises={parts[1].exercises} />
+    <Part part={parts[2].name} exercises={parts[2].exercises} />
   </div>
 )
 
-const Total = ({exercises1, exercises2, exercises3}) => (
+const Total = ({parts}) => (
   <div>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   </div>
 )
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]  
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
 }
