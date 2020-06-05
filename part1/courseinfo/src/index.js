0ae6692257a88902a146eb96fa21434c53d0f4dd
translucent504 <fleshlime.1@gmail.com>
2020-06-05 14:34:24 +0500

    step 2 refactor Content into Part component.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,17 +7,19 @@ const Header = ({course}) => (
   </div>
 )
 
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => (
+const Part = ({part, exercises}) => (
   <div>
     <p>
-      {part1} {exercises1}
+      {part} {exercises}
     </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+  </div>
+)
+
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => (
+  <div>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
   </div>
 )
 
