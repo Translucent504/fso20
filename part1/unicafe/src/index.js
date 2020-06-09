551111a637aa3e99463e36fbf32e8b2e5c79a7d9
translucent504 <fleshlime.1@gmail.com>
2020-06-10 01:23:59 +0500

    Completed step 2 and 3 Introduced statistics Refactored into separate component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,25 +7,36 @@ const Button = ({handleClick, text}) => {
     );
 }
 
+const Statistics = ({good, neutral, bad, all}) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        <li>all {all}</li>
+        <li>average {(1*good + 0*neutral - 1*bad)/all}</li>
+        <li>percentage {good/all * 100}%</li>
+      </ul>
+    </div>
+  );
+}
+
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good + neutral + bad
   return (
   <div>
     <h1>give feedback</h1>
     <Button handleClick={() => setGood(good + 1)} text="good"/>
     <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
     <Button handleClick={() => setBad(bad + 1)} text="bad"/>  
-    <h1>Statistics</h1>
-    <ul>
-      <li>good {good}</li>
-      <li>neutral {neutral}</li>
-      <li>bad {bad}</li>
-    </ul>      
+    <Statistics good={good} neutral={neutral} bad={bad} all={all}/>      
   </div>
   )
 }
