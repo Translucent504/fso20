c153ea814ec8f13a469a7ba2cef2aa12eff746bc
translucent504 <fleshlime.1@gmail.com>
2020-06-16 23:39:10 +0500

    part2 phonebook exercise 2.11 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import AddPerson from './components/AddPerson'
 import PersonList from './components/PersonList'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ])
+    const [persons, setPersons] = useState([])
     const [personFilter, setPersonFilter] = useState([...persons])
+    
+    useEffect(()=>{
+        axios
+        .get("http://localhost:3001/persons")
+        .then((response)=>{
+            setPersons(response.data)
+            setPersonFilter(response.data)
+        })
+    }, [])
 
     return (
         <div>
