75b357973ca2198278f634d0e149ba9db0cd1066
translucent504 <fleshlime.1@gmail.com>
2020-06-20 14:12:22 +0500

    fullstack

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "/api/persons/"
 
 const create = (newPerson) => {
     const response = axios.post(baseUrl, newPerson)
