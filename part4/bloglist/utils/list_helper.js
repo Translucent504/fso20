abf86cd9f5068df8f4dd7440100aa060aec13bd6
Translucent504 <fleshlime.1@gmail.com>
2020-07-02 00:06:41 +0500

    part4 4.4 helper function totalLikes test completed

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs.reduce((sum, blog) => {
+        return sum + blog.likes
+    }, 0)
+    return total
+}
+
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
