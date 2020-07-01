da977558c11ced0ef2b8ecb5a8403ace7c97db1e
Translucent504 <fleshlime.1@gmail.com>
2020-07-01 20:37:06 +0500

    part4 bloglist exercise 4.2 completed refactoring

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,15 +4,21 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = 'mongodb+srv://fullstack:infinitude@cluster0-skbal.mongodb.net/phonebook?retryWrites=true&w=majority'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Cant connect to MongoDB')
+  })
 
+// MIDDLEWARE ZONE
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -2,3 +2,7 @@ require('dotenv').config()
 
 PORT = process.env.PORT
 MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+    PORT, MONGODB_URI
+}
