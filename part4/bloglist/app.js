4ac3b84aa54b9ac24440520dfed957ad2fa3ce94
Translucent504 <fleshlime.1@gmail.com>
2020-07-01 20:10:30 +0500

    part4 bloglist refactored blog controller and blog models

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,31 +3,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = 'mongodb+srv://fullstack:infinitude@cluster0-skbal.mongodb.net/phonebook?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,23 @@
+const Blog = require('../models/blog')
+const { model } = require('../models/blog')
+const blogsRouter = require('express').Router()
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
