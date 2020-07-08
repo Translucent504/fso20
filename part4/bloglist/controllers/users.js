07113739f2b33c0ea448de58b771b3d5c1d6e051
Translucent504 <fleshlime.1@gmail.com>
2020-07-08 15:40:55 +0500

    part4d users get request done

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,6 +2,11 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users.map(u => u.toJSON()))
+})
+
 usersRouter.post('/', async (request, response) => {
     const body = request.body
     // username , password and name.
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -6,4 +6,12 @@ const userSchema = new mongoose.Schema({
     passwordHash: String,
 })
 
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id,
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 module.exports = mongoose.model('User', userSchema)
