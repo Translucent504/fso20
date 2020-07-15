e8d4444f3734c3ac50722bb9b411969fc8bcbda7
Translucent504 <fleshlime.1@gmail.com>
2020-07-15 14:58:40 +0500

    5.3 create new blogs when logged in

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -7,6 +7,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -14,6 +17,7 @@ const App = () => {
     if (storedUserJSON) {
       const user = JSON.parse(storedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -33,6 +37,7 @@ const App = () => {
       if (loggedUser) {
         window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(loggedUser))
         setUser(loggedUser)
+        blogService.setToken(loggedUser.token)
         setUsername('')
         setPassword('')
       }
@@ -44,8 +49,12 @@ const App = () => {
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
-      Username:<input type="text" value={username} onChange={({ target }) => setUsername(target.value)} /><br />
-      Password:<input type="password" value={password} onChange={({ target }) => setPassword(target.value)} /><br />
+      <div>
+        Username:<input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        Password:<input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+      </div>
       <button type="submit">Login</button>
     </form>
   )
@@ -54,6 +63,39 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
     setUser(null)
   }
+
+  const handleBlogCreate = async (event) => {
+    event.preventDefault()
+    const newBlog = {title, author, url}
+    try {
+      const response = await blogService.create(newBlog)
+      console.log(response)
+      setBlogs([...blogs, response])
+      setAuthor('')
+      setUrl('')
+      setTitle('')
+    } catch (error) {
+      console.log('invalid blog?')
+      setAuthor('')
+      setUrl('')
+      setTitle('')
+    }
+  }
+
+  const blogForm = () => (
+    <form onSubmit={handleBlogCreate}>
+      <div>
+        Title:<input value={title} onChange={({ target }) => setTitle(target.value)} type="text" />
+      </div>
+      <div>
+        Author:<input value={author} onChange={({ target }) => setAuthor(target.value)} type="text" />
+      </div>
+      <div>
+        Url:<input value={url} onChange={({ target }) => setUrl(target.value)} type="text" />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  )
 
 
   return (
@@ -64,11 +106,14 @@ const App = () => {
         : <div>
           <p>{user.username} Logged in</p>
           <button onClick={handleLogout}>Logout</button>
-          {
-            blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} />
-            )
-          }
+          <div>
+            {blogForm()}
+            {
+              blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+              )
+            }
+          </div>
         </div>
       }
 
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
 }
 
-export default { getAll }
+const create = async (blog) => {
+  // assuming that the blog is an object of the form {url, author, title}
+  const config = {
+    headers: {Authorization: token}
+  }
+  const response = await axios.post(baseUrl, blog, config)
+  return response.data 
+}
+
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+export default { getAll, create, setToken }
