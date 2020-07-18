0d06bbe2a7bc1a15b526498f1c718ff1b88de3e8
Translucent504 <fleshlime.1@gmail.com>
2020-07-18 20:35:11 +0500

    5.6 and 5.7 Hide Details and view details to separate Blog Component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React,{useState} from 'react'
+
+const Blog = ({ blog }) => {
+const [showDetails, setShowDetails] = useState(false)
+
+  const handleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+  
+  return (
+    <div>
+      {blog.title} {blog.author} <button onClick={handleShowDetails}>{showDetails ? 'Hide Details' : 'View Details'}</button>
+      <div style={{display: showDetails ? '':'none'}}>
+        <ul>
+          <li>{blog.url}</li>
+          <li>Likes: {blog.likes} <button>like</button></li>
+          <li>{blog.user.name}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
