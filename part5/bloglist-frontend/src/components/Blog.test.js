6cf70efdc111273c6f71a1dd67b84b8e2bcd2322
Translucent504 <fleshlime.1@gmail.com>
2020-07-24 13:31:37 +0500

    5.14 tested View details button to show the details on click.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,29 +1,46 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('blog rendered with only title and author and details are hidden', () => {
-  const user = { username : 'test username' }
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    url: 'test url',
-    likes: 0,
-    user: { name: 'test user name' }
-  }
+describe('<Blog />', () => {
+  let component;
 
-  const mockHandler = jest.fn()
+  beforeEach(() => {
+    const user = { username: 'test username' }
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'test url',
+      likes: 0,
+      user: { name: 'test user name' }
+    }
 
-  const component = render(
-    <Blog handleBlogUpdate={mockHandler} blog={blog} user={user}/>
-  )
+    const mockHandler = jest.fn()
 
-  const blogDiv = component.container.querySelector('.blogDiv')
-  const blogDetails = component.container.querySelector('.blogDetails')
+    component = render(
+      <Blog handleBlogUpdate={mockHandler} blog={blog} user={user} />
+    )
+  })
 
-  expect(blogDiv).toHaveTextContent(blog.title)
-  expect(blogDiv).toHaveTextContent(blog.author)
-  expect(blogDetails).toHaveStyle('display: none')
+  test('blog rendered with only title and author and details are hidden', () => {
+    const blogDiv = component.container.querySelector('.blogDiv')
+    const blogDetails = component.container.querySelector('.blogDetails')
+
+    expect(blogDiv).toHaveTextContent('test title')
+    expect(blogDiv).toHaveTextContent('test author')
+    expect(blogDetails).toHaveStyle('display: none')
+  })
+
+  test('clicking button shows blog details', () => {
+    const button = component.getByText('View Details')
+    const blogDetails = component.container.querySelector('.blogDetails')
+
+    fireEvent.click(button)
+
+    expect(blogDetails).not.toHaveStyle('display: none')
+  })
+  
 })
+
 
