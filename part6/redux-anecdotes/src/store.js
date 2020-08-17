e688e5a94fcb7de08bf7c1fdb677c640bdeda4ec
Translucent504 <fleshlime.1@gmail.com>
2020-08-17 18:52:41 +0500

    6.9 separated store and added redux devtools

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
+import anecdoteReducer from "./reducers/anecdoteReducer";
+import notificationReducer from "./reducers/notificationReducer";
+
+const reducer = combineReducers({
+    anecdotes: anecdoteReducer,
+    notification: notificationReducer
+})
+
+export const store = createStore(reducer, composeWithDevTools())
+
